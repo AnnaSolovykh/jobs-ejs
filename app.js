411e9d7a045bd76195aa3c0dee5dd1a9cd65d752
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-01-18 14:32:31 +0300

    persist data in mongo db and add flash messages

--- app.js
+++ app.js
@@ -9,45 +9,74 @@ app.use(require("body-parser").urlencoded({ extended: true }));
  // to load the .env file into the process.env object
 require("dotenv").config();
 const session = require("express-session");
-app.use(
-  session({
+const MongoDBStore = require("connect-mongodb-session")(session);
+const url = process.env.MONGO_URI;
+
+const store = new MongoDBStore({
+    uri: url,
+    collection: "mySessions",
+});
+store.on("error", function (error) {
+    console.log(error);
+});
+
+const sessionParms = {
     secret: process.env.SESSION_SECRET,
-    resave: false,
+    resave: true,
     saveUninitialized: true,
-  })
-);
+    store: store,
+    cookie: { secure: false, sameSite: "strict" },
+};
+
+if (app.get("env") === "production") {
+    app.set("trust proxy", 1); // trust first proxy
+    sessionParms.cookie.secure = true; // serve secure cookies
+}
+
+app.use(session(sessionParms));
+
+app.use(require("connect-flash")());
 
 // secret word handling
 app.get("/secretWord", (req, res) => {
     if (!req.session.secretWord) {
-      req.session.secretWord = "syzygy";
+        req.session.secretWord = "syzygy";
     }
+    res.locals.info = req.flash("info");
+    res.locals.errors = req.flash("error");
     res.render("secretWord", { secretWord: req.session.secretWord });
 });
+
 app.post("/secretWord", (req, res) => {
-    req.session.secretWord = req.body.secretWord;
+    if (req.body.secretWord.toUpperCase()[0] == "P") {
+        req.flash("error", "That word won't work!");
+        req.flash("error", "You can't use words that start with p.");
+    } else {
+        req.session.secretWord = req.body.secretWord;
+        req.flash("info", "The secret word was changed.");
+    }
     res.redirect("/secretWord");
 });
 
 app.use((req, res) => {
-  res.status(404).send(`That page (${req.url}) was not found.`);
+    res.status(404).send(`That page (${req.url}) was not found.`);
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).send(err.message);
-  console.log(err);
+    res.status(500).send(err.message);
+    console.log(err);
 });
 
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        app.listen(port, () =>
+        console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
