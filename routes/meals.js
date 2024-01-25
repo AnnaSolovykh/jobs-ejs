f530f3f46a27744580ce40dc17ac45b9e71f45dc
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-01-25 18:51:02 +0300

    update comments in meals router

--- routes/meals.js
+++ routes/meals.js
@@ -10,22 +10,22 @@ const {
     deleteMeal
 } = require("../controllers/meals");
 
-//GET /jobs (display all the job listings belonging to this user)
-//POST /jobs (Add a new job listing)
+//GET display all the meal listings belonging to this user
+//POST add a new meal listing
 router.route("/meals")
     .get(getMeals)
     .post(addMeal);
 
-//GET /jobs/new (Put up the form to create a new entry)
+//GET put up the form to create a new entry
 router.route("/meals/new").get(getNewMealForm);
 
-//GET /jobs/edit/:id (Get a particular entry and show it in the edit box)
-router.route("/edit/:id").get(editMeal);
+//GET get a particular entry and show it in the edit box
+router.route("/meals/edit/:id").get(editMeal);
 
-//POST /jobs/update/:id (Update a particular entry)
-router.route("/update/:id").post(updateMeal);
+//POST update a particular entry
+router.route("/meals/update/:id").post(updateMeal);
 
-//POST /jobs/delete/:id (Delete an entry)
-router.route("/delete/:id").post(deleteMeal);
+//POST delete an entry
+router.route("/meals/delete/:id").post(deleteMeal);
 
 module.exports = router;
