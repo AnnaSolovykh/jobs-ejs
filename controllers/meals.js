1d02cc00c0906ce24da7006b9604fe598de85044
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-01-25 18:50:44 +0300

    update meals controllers

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,28 +1,133 @@
-const Meal = require('../models/meal');
+const Meal = require("../models/meal");
+const parseValidationErrors = require("../utils/parseValidationErrs");
 
+// GET all meals for the current user
 const getMeals = async (req, res, next) => {
-    res.send("getMeals function called")
-}
+    try {
+        const meals = await Meal.find({ createdBy: req.user._id });
+        res.render('meals', { meals });
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            const errors = parseValidationErrors(error);
+            req.flash('error', errors);
+        } else {
+            res.status(500);
+            req.flash('error', 'An internal server error occurred.');
+        }
+    }
+};
 
+// POST a new meal
 const addMeal = async (req, res, next) => {
-    res.send("addMeal function called")
-}
+    // Convert 'isFavorite' from 'on'/'undefined' to true/false
+    req.body.isFavorite = req.body.isFavorite === 'on';
+    try {
+        const newMeal = await Meal.create({ ...req.body, createdBy: req.user._id });
+        res.redirect('/meals'); 
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            const errors = parseValidationErrors(error);
+            req.flash('error', errors);
+        } else {
+            res.status(500);
+            req.flash('error', 'An internal server error occurred.');
+            res.redirect('/meals');
+        }
+    }
+};
 
+// GET the form for adding a new meal
 const getNewMealForm = async (req, res) => {
-    res.send("getNewMealForm function called")
-}
+    try {
+        res.render('newMealForm', {}); 
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            const errors = parseValidationErrors(error);
+            req.flash('error', errors);
+            res.redirect('/meals');
+        } else {
+            res.status(500);
+            req.flash('error', 'An internal server error occurred.');
+            res.redirect('/meals');
+        }
+    }
+};
 
+
+// GET a specific meal for editing
 const editMeal = async (req, res, next) => {
-    res.send("editMeal function called")
-}
+    try {
+        const meal = await Meal.findOne({ _id: req.params.id, createdBy: req.user._id });
+        if (!meal) {
+            res.status(404);
+            req.flash('error', 'Meal not found');
+            return;
+        }
+        res.render('meal', { meal }); 
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            const errors = parseValidationErrors(error);
+            req.flash('error', errors);
+            res.redirect('/meals');
+        } else {
+            res.status(500);
+            req.flash('error', 'An internal server error occurred.');
+            res.redirect('/meals');
+        }
+    }
+};
 
+// POST an updated meal
 const updateMeal = async (req, res, next) => {
-    res.send("updateMeal function called")
-}
+    // Convert 'isFavorite' from 'on'/'undefined' to true/false
+    req.body.isFavorite = req.body.isFavorite === 'on';
+    try {
+        const updatedMeal = await Meal.findOneAndUpdate(
+            { _id: req.params.id, createdBy: req.user._id },
+            req.body,
+            { new: true, runValidators: true }
+        );
+        if (!updatedMeal) {
+            res.status(404);
+            req.flash('error', 'Meal not found');
+            return;
+        }
+        res.redirect('/meals');
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            const errors = parseValidationErrors(error);
+            req.flash('error', errors);
+            res.redirect('/meals/edit/' + req.params.id);
+        } else {
+            res.status(500);
+            req.flash('error', 'An internal server error occurred.');
+        }
+    }
+};
 
+// POST to delete a meal
 const deleteMeal = async (req, res, next) => {
-    res.send("deleteMeal function called")
-}
+    try {
+        const deletedMeal = await Meal.findOneAndDelete({ _id: req.params.id, createdBy: req.user._id });
+        if (!deletedMeal) {
+            res.status(404);
+            req.flash('error', 'Meal not found');
+            return res.redirect('/meals'); 
+        }
+        req.flash('success', 'Meal was deleted');
+        res.redirect('/meals');
+    } catch (error) {
+        if (error.name === 'ValidationError') {
+            const errors = parseValidationErrors(error);
+            req.flash('error', errors);
+            res.redirect('/meals/edit/' + req.params.id);
+        } else {
+            res.status(500);
+            req.flash('error', 'An internal server error occurred.');
+            return res.redirect('/meals');
+        }
+    }
+};
 
 module.exports = {
     getMeals,
